59b6ffc25e2bbb8f0aee378cec6a3365e8f6f296
hermione <134724405+komaljain1099@users.noreply.github.com>
2024-03-10 23:56:58 +0530

    Add files via upload

---
+++ _Day1 Datatypes1.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Jupyter shortcuts
+# + esc + a - Add a cell above the current cell
+# + esc + b - Add a cell below the current cell
+# + esc + m - Convert current cell to markdown
+# + esc + y - Convert current cell to code cell
+# + esc + x - Delete the current cell
+#     
+
+# In[ ]:
+
+
+
+
+
+# ## Datatypes
+
+# In[1]:
+
+
+# Integer 
+x = 10
+
+
+# In[2]:
+
+
+print(x)
+
+
+# In[3]:
+
+
+type(x)
+
+
+# In[4]:
+
+
+# float
+y = 12.8
+
+
+# In[5]:
+
+
+y
+
+
+# In[6]:
+
+
+type(y)
+
+
+# In[7]:
+
+
+# Boolean
+z = True
+
+
+# In[8]:
+
+
+z
+
+
+# In[9]:
+
+
+type(z)
+
+
+# In[10]:
+
+
+x = 10
+y = 3
+
+
+# In[11]:
+
+
+# Addittion 
+x + y
+
+
+# In[12]:
+
+
+# Subtraction
+x - y
+
+
+# In[13]:
+
+
+# Multiplication
+x * y
+
+
+# In[14]:
+
+
+# Division
+x / y
+
+
+# In[15]:
+
+
+# Modules
+x % y
+
+
+# In[16]:
+
+
+# Power 
+x ** y
+
+
+# In[17]:
+
+
+# Floor Division
+x // y
+
+
+# In[18]:
+
+
+-x//y
+
+
+# ## Logical Operations
+
+# In[19]:
+
+
+x = True
+y = False 
+
+
+# In[20]:
+
+
+x or y
+
+
+# In[21]:
+
+
+x and y
+
+
+# In[22]:
+
+
+not x
+
+
+# In[23]:
+
+
+not y
+
+
+# In[ ]:
+
+
+
+
